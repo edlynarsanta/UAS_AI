4d62184f50c1aaa6d79dad685ae92191c5522fe9
edlynarsanta <picaarsantaputra@gmail.com>
2025-06-21 10:25:16 +0800

    Create Representasi Data Lalu Lintas Sederhana.py

---
+++ Smart Traffic Mangement/Representasi Data Lalu Lintas Sederhana.py
@@ -0,0 +1,25 @@
+# Misal ini adalah data yang diterima dari satu persimpangan (misal ada 4 jalur/arah)
+data_persimpangan = {
+    "persimpangan_id": "P001",
+    "waktu_timestamp": "2025-06-21 10:30:00",
+    "arah_utara": {
+        "volume_kendaraan": 120, # jumlah kendaraan per menit
+        "kecepatan_rata_rata": 35, # km/jam
+        "panjang_antrean": 50 # meter
+    },
+    "arah_timur": {
+        "volume_kendaraan": 80,
+        "kecepatan_rata_rata": 40,
+        "panjang_antrean": 30
+    },
+    "arah_selatan": {
+        "volume_kendaraan": 150,
+        "kecepatan_rata_rata": 25,
+        "panjang_antrean": 70
+    },
+    "arah_barat": {
+        "volume_kendaraan": 90,
+        "kecepatan_rata_rata": 38,
+        "panjang_antrean": 20
+    }
+}
